2a7390b7c2c44601acbe2de076953433de7d4ab4
orgamiryazdani <orgamiryazdani@gmail.com>
2025-06-05 09:27:51 +0330

    feat: Add suspense

--- src/app/(private)/dashboard/products/[id]/page.tsx
+++ src/app/(private)/dashboard/products/[id]/page.tsx
@@ -1,11 +1,15 @@
+import Spinner from '@/components/Spinner';
 import ProductDetailView from '@/modules/products/views/ProductDetailView';
+import { Suspense } from 'react';
 
 async function page({ params }: { params: Promise<{ id: string }> }) {
   const data = await params;
   const { id } = data;
   return (
     <div>
-      <ProductDetailView id={id} />
+      <Suspense fallback={<Spinner />}>
+        <ProductDetailView id={id} />
+      </Suspense>
     </div>
   );
 }
