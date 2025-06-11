008a06f2180e797bda52f4daf2d9b2cf97993f4f
orgamiryazdani <orgamiryazdani@gmail.com>
2025-06-11 08:32:38 +0330

    feat: Add metadata, sitemap, schema, robots for seo

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,12 +1,51 @@
 import { ProductsWithImages } from '@/types';
 import ProductDetail from '@/modules/products/components/ProductDetail';
+import customMetadataGenerator from '@/lib/metadata';
 import { getProductsById } from '@/modules/products/services';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const data = await params;
+  const { id } = data;
+  const product = (await getProductsById(id)) as ProductsWithImages;
+  if (!product) {
+    return customMetadataGenerator({
+      title: 'not found',
+    });
+  }
+
+  return customMetadataGenerator({
+    title: product?.name,
+    description: product?.description,
+    images: product?.images,
+  });
+}
 
 async function page({ params }: { params: Promise<{ id: string }> }) {
   const data = await params;
   const { id } = data;
   const product = (await getProductsById(id)) as ProductsWithImages;
-  return <ProductDetail {...product} />;
+
+  const jsonLd = {
+    '@context': 'http://schema.org',
+    '@type': 'Product',
+    name: product.name,
+    image: product?.images.length && product.images[0].image,
+    description: product.description,
+  };
+
+  return (
+    <section>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <ProductDetail {...product} />;
+    </section>
+  );
 }
 
 export default page;
